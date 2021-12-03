1c42c7a92aa79a63876776101efd6338f72f9f2b
romanmasalskiy <romanmasalskiy@gmail.com>
2021-12-03 20:18:54 +0300

    cors

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const SneaksAPI = require('./controllers/sneaks.controllers.js');
 
 var port = process.env.PORT || 4000;
 mongoose.Promise = global.Promise;
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 /*Sneaker.deleteMany({ }, function (err) {
   if(err) console.log(err);
   console.log("Successful deletion");
